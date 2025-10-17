6bc3af09cfcf01175530700fe504a542129f6b76
jsadg <jstoll3893@gmail.com>
2025-10-16 20:06:16 -0400

    Move encoding and basic pawn moves

--- engine.c
+++ engine.c
@@ -749,12 +749,54 @@ static inline void generate_moves(){
 
         //Generate white pawns and castling moves
         if(side == white){
-            
+            if(piece == P){
+                //Handle move forward by one
+                while(bitboard){
+                    source_square = get_lsb_index(bitboard);
+                    target_square = source_square - 8;
+                    if(!(target_square < a8) && !get_bit(occupancies[both], target_square)){
+                        //Pawn promotion
+                        if(source_square >= a7 && source_square <= h7){
+                            //Pawn promotion
+                        }
+                        else{
+                            //One square pawn move
+
+                            //Two square pawn move
+                            if((source_square >= a2 && source_square <= h2) && !get_bit(occupancies[both], target_square)){
+
+                            }
+                        }
+                    }
+                    pop_bit(get_lsb_index(bitboard));
+                }
+            }
         }
 
         //Generate black pawns and castling moves
-        if(side == black){
-            
+        else{
+            if(piece == p){
+                //Handle move forward by one
+                while(bitboard){
+                    source_square = get_lsb_index(bitboard);
+                    target_square = source_square + 8;
+                    if(!(target_square < a8) && !get_bit(occupancies[both], target_square)){
+                        //Pawn promotion
+                        if(source_square >= a2 && source_square <= h2){
+                            //Pawn promotion
+                        }
+                        else{
+                            //One square pawn move
+                            
+                            //Two square pawn move
+                            if((source_square >= a7 && source_square <= h7) && !get_bit(occupancies[both], target_square)){
+
+                            }
+                        }
+                    }
+                    pop_bit(get_lsb_index(bitboard));
+                }
+            }
         }
 
         //Generate knight moves
@@ -769,6 +811,17 @@ static inline void generate_moves(){
     }
 }
 
+/*
+                binary representation                   hexidecimal constants
+  0000 0000 0000 0000 0011 1111    source square        0x3f
+  0000 0000 0000 1111 1100 0000    target square        0xfc0
+  0000 0000 1111 0000 0000 0000    piece                0xf000
+  0000 1111 0000 0000 0000 0000    promoted piece       0xf0000
+  0001 0000 0000 0000 0000 0000    capture flag         0x100000
+  0010 0000 0000 0000 0000 0000    double push flag     0x200000
+  0100 0000 0000 0000 0000 0000    enpassant flag       0x400000
+  1000 0000 0000 0000 0000 0000    castling flag        0x800000
+*/
 
 int main(){
     init_piece_attack_tables();
